353f33211a2be5c5c0abf16493b656447011f78c
John L <johnnyl@email.com>
2020-11-08 13:48:43 -0600

    Test

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    
+    <h1>Would you rather...</h1>
+
+    <would-you-rather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"></would-you-rather>
+
+    <p>{{ userSelectionMessage }}</p>
+
+    <!-- would-you-rather is listening for answer-changed event, answerChanged is method created below-->
+    <!-- <WouldYouRather></WouldYouRather> will work too. Above is the name of the file-->
+
+  </div>
+</template>
+
+<script>
+// App.vue is sending data to WouldYouRather via props v-bind data. WouldYouRather is comminucating to its parent by emitting an event
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return{
+      wyrQuestion: 'Would you rather lose the ability to lie or believe everything you’re told?',
+      wyrAnswer1: 'Lose ability to lie',
+      wyrAnswer2: 'Believe everything you\'re told',
+      userSelectionMessage: '' // Empty because user hasn't chose anthing yet
+    }
+  },
+  methods: {
+    answerChanged(choice) { // arguement to this method "choice" is being sent from this.choice from WouldYouRather
+      this.userSelectionMessage = `Thanks you chose ${choice}`
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: lightskyblue;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: lightgreen;
+}
+</style>
+
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wyr">
+    
+    <h2>Please make your choice!</h2>
+
+    <h3>{{ question }}</h3>
+
+    <!-- v-bind sets radio buttons value -->
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade"> 
+    <label>{{ answer1 }}</label>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather', // App.vue is the parent
+  props: { // props and data treated similarly, prop provided by parent
+    question: String,
+    answer1: String,
+    answer2: String
+    // Never modify prop in a component
+  },
+  data() { // data is a function that returns an object of the above components data
+    return {
+      choice: '' // Empty string to represent no choice was made
+    }
+  },
+  methods: {
+    choiceMade() {
+      // emit a message named 'answer-change' this is an event
+      // dashes for event names are good because it won't conflict with a JavaScript name
+      // this.choice is listing data that we want sent as a part of the message to the parent 
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.wyr {
+  border: 2px black solid;
+  background: lightcyan;
+  margin: 10px;
+}
+</style>
